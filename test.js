813df5132971c37e159ece41539340d349983594
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2019-06-28 10:55:48 -0700

    Update copyright :D

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-// Copyright 2016 Yahoo Inc.
+// Copyright 2019 SmugMug, Inc.
 // Licensed under the terms of the MIT license. Please see LICENSE file in the project root for terms.
 
 var config = require(__dirname);
@@ -9,15 +9,11 @@ var assert = require('assert');
 describe('🔵🔴', function () {
 
   it('extends eslint:recommended', function () {
-
     assert.equal(config.extends, 'eslint:recommended');
-
   });
 
   it('exports default rules', function () {
-
     assert.equal(typeof config.rules, 'object');
-
   });
 
 });
